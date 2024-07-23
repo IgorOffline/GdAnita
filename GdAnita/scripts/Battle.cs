4d019df45171e0ad197b8e929b717e273d3f71af
Igor Durbek <igor@igordurbek.com>
2024-07-23 12:18:44 +0200

    gui: texture button

--- GdAnita/scripts/Battle.cs
+++ GdAnita/scripts/Battle.cs
@@ -4,11 +4,14 @@ using System;
 public partial class Battle : Node3D
 {
     private Button? _btnTeam1RPA1;
+    private TextureButton? _btnCard1;
 
     public override void _Ready()
     {
-        _btnTeam1RPA1 = GetNode<Button>("Control/Canvas/VBoxTeam1/BtnTeam1RPA1");
+        _btnTeam1RPA1 = GetNode<Button>("Canvas/GridTeam1/VBox1/HBoxRP/BtnRPA1");
         _btnTeam1RPA1.Pressed += () => { GD.Print("_btnTeam1RPA1"); };
+        _btnCard1 = GetNode<TextureButton>("Canvas/GridTeam1/HBoxCards/BtnCard1");
+        _btnCard1.Pressed += () => { GD.Print("_btnCard1"); };
     }
 
     public override void _Process(double delta)
