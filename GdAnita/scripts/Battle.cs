6e0b91d1bebdbd2220888bcae243f4468d7f5bb7
Igor Durbek <igor@igordurbek.com>
2024-07-24 09:44:24 +0200

    draw up to 3 cards

--- GdAnita/scripts/Battle.cs
+++ GdAnita/scripts/Battle.cs
@@ -12,6 +12,8 @@ public partial class Battle : Node3D
     private Label? _lblTeam2Hp;
     private Button? _btnTeam1ManaA;
     private TextureButton? _btnCard1;
+    private TextureButton? _btnCard2;
+    private TextureButton? _btnCard3;
     private CompressedTexture2D? _btnCardTextureNormalEmpty;
     private CompressedTexture2D? _btnCardTextureNormalHeld;
     private AudioStreamPlayer3D? _audioStreamPlayerCasting;
@@ -21,47 +23,43 @@ public partial class Battle : Node3D
     private uint _groundMask;
 
     private double _raycastTimer;
-    private double _raycastTimerMax = 0.25;
+    private double _raycastTimerMax = 0.11;
     private bool _doRaycast;
     private double _manaTimer;
-    private double _manaTimerMax = 0.25;
+    private double _manaTimerMax = 0.11;
     private double _handTimer;
-    private double _handTimerMax = 0.25;
+    private double _handTimerMax = 0.11;
 
     public override void _Ready()
     {
         _global = GetNode<Global>("/root/Global");
         _cam = GetNode<Camera3D>("Camera");
         _groundMask = GetNode("Slots/Team1Creature1").GetNode<StaticBody3D>("StaticBody3D").CollisionLayer;
-
-        GameMaster.Team1.ManaA = 6;
-
         _lblTeam1Hp = GetNode<Label>("Canvas/GridTeam1/VBox1/LblHP");
         _lblTeam2Hp = GetNode<Label>("Canvas/GridTeam2/VBox1/LblHP");
         _btnTeam1ManaA = GetNode<Button>("Canvas/GridTeam1/VBox1/HBoxMana/BtnManaA");
         var btnTeam2ManaA = GetNode<Button>("Canvas/GridTeam2/VBox1/HBoxMana/BtnManaA");
-        btnTeam2ManaA.Text = GameMaster.Team2.ManaA.ToString();
-
         var btnTeam1ManaB = GetNode<Button>("Canvas/GridTeam1/VBox1/HBoxMana/BtnManaB");
-        btnTeam1ManaB.Text = GameMaster.Team1.ManaB.ToString();
         var btnTeam1ManaC = GetNode<Button>("Canvas/GridTeam1/VBox1/HBoxMana/BtnManaC");
-        btnTeam1ManaC.Text = GameMaster.Team1.ManaC.ToString();
-
         var btnTeam2ManaB = GetNode<Button>("Canvas/GridTeam2/VBox1/HBoxMana/BtnManaB");
-        btnTeam2ManaB.Text = GameMaster.Team1.ManaB.ToString();
         var btnTeam2ManaC = GetNode<Button>("Canvas/GridTeam2/VBox1/HBoxMana/BtnManaC");
+        _btnCard1 = GetNode<TextureButton>("Canvas/GridTeam1/HBoxCards/BtnCard1");
+        _btnCard2 = GetNode<TextureButton>("Canvas/GridTeam1/HBoxCards/BtnCard2");
+        _btnCard3 = GetNode<TextureButton>("Canvas/GridTeam1/HBoxCards/BtnCard3");
+        _audioStreamPlayerCasting = GetNode<AudioStreamPlayer3D>("AudioStreamPlayerCasting");
+        _audioStreamPlayerPayCost = GetNode<AudioStreamPlayer3D>("AudioStreamPlayerPayCost");
+
+        _btnCardTextureNormalEmpty = GD.Load<CompressedTexture2D>("res://textures/anitabrown.png");
+        _btnCardTextureNormalHeld = GD.Load<CompressedTexture2D>("res://textures/anitagreen1.png");
+
+        GameMaster.Team1.ManaA = 6;
+
+        btnTeam2ManaA.Text = GameMaster.Team2.ManaA.ToString();
+        btnTeam1ManaB.Text = GameMaster.Team1.ManaB.ToString();
+        btnTeam1ManaC.Text = GameMaster.Team1.ManaC.ToString();
+        btnTeam2ManaB.Text = GameMaster.Team1.ManaB.ToString();
         btnTeam2ManaC.Text = GameMaster.Team1.ManaC.ToString();
 
-        _btnTeam1ManaA.Pressed += () =>
-        {
-            var costSuccessfullyPayed = GameMaster.Team1.PayManaA();
-            if (costSuccessfullyPayed)
-            {
-                _audioStreamPlayerPayCost!.Play();
-            }
-        };
-
-        _btnCard1 = GetNode<TextureButton>("Canvas/GridTeam1/HBoxCards/BtnCard1");
         _btnCard1.Pressed += () =>
         {
             var successfulTransition = GameMaster.Team1.CastSpell(new CardIndex(0));
@@ -71,11 +69,14 @@ public partial class Battle : Node3D
             }
         };
 
-        _btnCardTextureNormalEmpty = GD.Load<CompressedTexture2D>("res://textures/anitabrown.png");
-        _btnCardTextureNormalHeld = GD.Load<CompressedTexture2D>("res://textures/anitagreen1.png");
-        
-        _audioStreamPlayerCasting = GetNode<AudioStreamPlayer3D>("AudioStreamPlayerCasting");
-        _audioStreamPlayerPayCost = GetNode<AudioStreamPlayer3D>("AudioStreamPlayerPayCost");
+        _btnTeam1ManaA.Pressed += () =>
+        {
+            var costSuccessfullyPayed = GameMaster.Team1.PayManaA();
+            if (costSuccessfullyPayed)
+            {
+                _audioStreamPlayerPayCost!.Play();
+            }
+        };
     }
 
     public override void _Process(double delta)
@@ -106,18 +107,28 @@ public partial class Battle : Node3D
 
         if (_handTimer > _handTimerMax)
         {
-            if (GameMaster.Team1.Hand.Count > 0)
+            var buttons = new[]
             {
-                _btnCard1!.TextureNormal = _btnCardTextureNormalHeld;
-            }
-            else
-            {
-                _btnCard1!.TextureNormal = _btnCardTextureNormalEmpty;
-            }
+                _btnCard1!, _btnCard2!, _btnCard3!
+            };
             
+            for (var i = 0; i < buttons.Length; i++)
+            {
+                var button = buttons[i];
+
+                if (GameMaster.Team1.Hand.Count > i)
+                {
+                    button.TextureNormal = _btnCardTextureNormalHeld;
+                }
+                else
+                {
+                    button.TextureNormal = _btnCardTextureNormalEmpty;
+                }
+            }
+
             _handTimer = 0;
         }
-        
+
         ImGui.Begin("Battle");
         if (ImGui.Button("Draw card"))
         {
